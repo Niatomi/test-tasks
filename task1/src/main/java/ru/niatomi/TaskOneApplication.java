86da58a1259786ef2dc16633efd94f73cdcaa746
Niatomi <playervoker@gmail.com>
2022-08-06 15:02:42 +0400

    Renamed

--- task1/src/main/java/ru/niatomi/TaskOneApplication.java
+++ task1/src/main/java/ru/niatomi/TaskOneApplication.java
@@ -7,8 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author niatomi
  */
 @SpringBootApplication
-public class DemoApplication {
+public class TaskOneApplication {
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(TaskOneApplication.class, args);
     }
 }
