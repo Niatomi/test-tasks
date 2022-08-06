f57b232486fc40cf10ebea25fb51333eaec6f1ed
Niatomi <playervoker@gmail.com>
2022-08-06 20:37:56 +0400

    Edit mapping for calcEquation

--- task2/src/main/java/ru/niatomi/controller/CalcController.java
+++ task2/src/main/java/ru/niatomi/controller/CalcController.java
@@ -1,10 +1,7 @@
 package ru.niatomi.controller;
 
-import org.json.JSONObject;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import ru.niatomi.calc.wsdl.GetEquationResponse;
 
 /**
  * @author niatomi
@@ -13,7 +10,7 @@ import ru.niatomi.calc.wsdl.GetEquationResponse;
 @RequestMapping("/calc")
 public interface CalcController {
 
-    @GetMapping
+    @GetMapping("/")
     ResponseEntity<String> calcEquation(
             @RequestParam Integer a,
             @RequestParam Integer b,
