c83e8a73b0c65c0595a5ca0996644fa22b903070
Niatomi <playervoker@gmail.com>
2022-08-06 15:02:51 +0400

    Initial commit

---
+++ task2/src/main/java/ru/niatomi/TaskTwoApplication.java
@@ -0,0 +1,14 @@
+package ru.niatomi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author niatomi
+ */
+@SpringBootApplication
+public class TaskTwoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TaskTwoApplication.class, args);
+    }
+}
