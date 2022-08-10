ed3d061698038b646c08b25d537f6dbb65988548
Niatomi <playervoker@gmail.com>
2022-08-10 16:58:00 +0400

    Add D and formula to exception

--- task1/src/main/java/ru/niatomi/exceptions/DiscriminantBelowZeroException.java
+++ task1/src/main/java/ru/niatomi/exceptions/DiscriminantBelowZeroException.java
@@ -1,10 +1,19 @@
 package ru.niatomi.exceptions;
 
+import lombok.Getter;
+
 /**
  * @author niatomi
  */
+@Getter
 public class DiscriminantBelowZeroException extends Exception{
-    public DiscriminantBelowZeroException(Integer value) {
-        super("Discriminant must be equal or higher than zero, but " + value);
+
+    private Double D;
+    private String formula;
+
+    public DiscriminantBelowZeroException(Double d, String formula) {
+        super("Discriminant must be equal or higher than zero, but " + d);
+        this.D = d;
+        this.formula = formula;
     }
 }
